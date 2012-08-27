b6f15676f6b94baa1452191964cc8d608bddc10c
Jan Friesse <jfriesse@redhat.com>
2012-08-27 18:07:13 +0200

    Add test case for nonexisting log file directory

---
+++ tests/logfile-open-error-str.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Author: Jan Friesse <jfriesse@redhat.com>
+#
+
+test_description="Test that error message is properly saved"
+
+. common.sh
+
+generate_corosync_conf_cb() {
+    sed 's/^[ \t]*logfile: .*$/logfile: \/nonexistingdir\/corosync.log/'
+}
+
+configure_corosync "$nodes_ip"
+start_corosync "$nodes_ip" && exit 1
+start_corosync "$nodes_ip" 2>&1 | grep -i 'no such file or directory'
+
+exit 0
