a89e4bee8323cdc965309875ccf453e4dd815390
Jan Friesse <jfriesse@redhat.com>
2012-09-27 12:57:44 +0200

    Add test for big ringnumber segfault

---
+++ tests/big-ringnumber-segfault.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Author: Jan Friesse <jfriesse@redhat.com>
+#
+
+test_description="Test that big ringnumber in config doesn't result in segfault"
+
+. common.sh
+
+generate_corosync_conf_cb() {
+    sed 's/^[ \t]*ringnumber: .*$/ringnumber: 100/'
+}
+
+configure_corosync "$nodes_ip"
+start_corosync "$nodes_ip" && exit 1 || res=$?
+[ "$res" -gt 127 ] && exit 1 || true
+
+cat_corosync_log "$nodes_ip" | grep "interface ring number .* is bigger then allowed maximum "
+
+exit 0
