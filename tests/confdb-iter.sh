2c4dc7a21523a953e5ca42393196cfb1800ef68d
Jan Friesse <jfriesse@redhat.com>
2012-12-06 08:41:26 +0100

    Add confdb-iter test

---
+++ tests/confdb-iter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Author: Jan Friesse <jfriesse@redhat.com>
+#
+
+test_description="Test confdb - iteration operations"
+
+. common.sh
+
+configure_corosync "$nodes_ip"
+start_corosync "$nodes_ip"
+
+if run "$nodes_ip" 'cat /usr/include/corosync/confdb.h' &>/dev/null;then
+    compile_app "$nodes_ip" "confdb-iter" "-lconfdb"
+    run_app "$nodes_ip" 'confdb-iter'
+else
+    compile_app "$nodes_ip" "cmap-iter" "-lcmap"
+    run_app "$nodes_ip" 'cmap-iter'
+fi
+
+exit 0
