f03e612d2273734bf2c61082173d983a3858b196
Jan Friesse <jfriesse@redhat.com>
2013-03-29 09:08:39 +0100

    fplay-segfault: Add support for needle

--- tests/fplay-segfault.sh
+++ tests/fplay-segfault.sh
@@ -4,14 +4,20 @@
 #
 
 test_description="Test that replaying randomly corrupted fdata always ends and doesn't segfault"
-test_corover_flatiron_enabled=true
-test_corover_needle_enabled=false
+test_corover_undefined_enabled=false
 
 . inc/common.sh
 . inc/cpg-load.sh
 
+fplay_app="corosync-fplay"
+output_file="/var/lib/corosync/fdata"
+if [ "$corosync_version" == "needle" ];then
+    fplay_app="qb-blackbox"
+    fplay_params="$output_file"
+fi
+
 exit_trap_end_cb() {
-    run "$nodes_ip" "killall -9 corosync-fplay || true"
+    run "$nodes_ip" "killall -9 $fplay_app || true"
 }
 
 # Generate valid fdata file
@@ -30,11 +36,11 @@ compile_app "$nodes_ip" "file-change-bytes"
 
 run "$nodes_ip" "cd ~/csts-apps; \
     for ((i=0; i<1024; i++));do \
-        ./file-change-bytes -i /var/lib/corosync/fdata-correct -o /var/lib/corosync/fdata -n 1024; \
-        corosync-fplay &>/dev/null; \
+        ./file-change-bytes -i /var/lib/corosync/fdata-correct -o $output_file -n 1024; \
+        $fplay_app $fplay_params &>/dev/null; \
         [ "'"$?"'" -gt 127 ] && exit 1 || true; \
     done"
 
-run "$nodes_ip" "rm -f /var/lib/corosync/fdata"
+run "$nodes_ip" "rm -f $output_file"
 
 exit 0
