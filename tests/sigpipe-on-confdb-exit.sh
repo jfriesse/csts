c12c59ff9ae43cdd3864beae78370204f19c41fe
Jan Friesse <jfriesse@redhat.com>
2013-05-02 16:19:41 +0200

    Add test with sigpipe

---
+++ tests/sigpipe-on-confdb-exit.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Author: Jan Friesse <jfriesse@redhat.com>
+#
+
+test_description="Test that closing confdb client in connecting state doesn't end with SIGPIPE in daemon"
+test_corover_undefined_enabled=false
+
+. inc/common.sh
+
+pids=""
+
+if [ "$corosync_version" == "flatiron" ];then
+    compile_app "$nodes_ip" "confdb-track-and-change" "-DUSE_CONFDB -lconfdb"
+fi
+
+if [ "$corosync_version" == "needle" ];then
+    compile_app "$nodes_ip" "confdb-track-and-change" "-DUSE_CMAP -lcmap"
+fi
+
+configure_corosync "$nodes_ip"
+
+# Start (foreground) corosync in background
+start_corosync_insert_marker "$nodes_ip"
+run $nodes_ip 'corosync -f -p' &
+coropid=$!
+start_corosync_wait_for_start "$nodes_ip"
+
+# Exec app
+run_app "$nodes_ip" 'confdb-track-and-change -u -n 1 -c 64' &
+pid=$!
+sleep 5
+
+# Kill app
+run "$nodes_ip" "killall -INT confdb-track-and-change"
+wait $pid
+
+stop_corosync "$nodes_ip"
+
+exit 0
