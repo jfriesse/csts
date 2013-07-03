91bde1ec70d8ad6bbaa3c88a0821d5a02b2253b2
Jan Friesse <jfriesse@redhat.com>
2013-07-03 11:57:09 +0200

    Use cpg-cli-client in verbose-procjoin test

--- tests/verbose-procjoin.sh
+++ tests/verbose-procjoin.sh
@@ -11,12 +11,12 @@ generate_corosync_conf_cb() {
     sed 's/^[ \t]*debug: .*$/debug: on/'
 }
 
-compile_app "$nodes_ip" "testcpg" "-lcpg"
+compile_app "$nodes_ip" "cpg-cli-client" "-lcpg"
 
 configure_corosync "$nodes_ip"
 start_corosync "$nodes_ip"
 
-echo "EXIT" | run_app "$nodes_ip" 'testcpg' > /dev/null
+echo -e "sync\nexit\n" | run_app "$nodes_ip" 'cpg-cli-client'
 cat_corosync_log "$nodes_ip" | grep 'got procjoin message from cluster node .* for pid .*'
 
 exit 0
