f481e7ce905768747d9432d8185cd3da359822d1
Jan Friesse <jfriesse@redhat.com>
2013-07-03 12:22:15 +0200

    Use cpg-cli-client in confdb-dispatch-notify test

--- tests/confdb-dispatch-notify.sh
+++ tests/confdb-dispatch-notify.sh
@@ -11,14 +11,14 @@ generate_corosync_conf_cb() {
     sed 's/^[ \t]*debug: .*$/debug: on/'
 }
 
-compile_app "$nodes_ip" "testcpg" "-lcpg"
+compile_app "$nodes_ip" "cpg-cli-client" "-lcpg"
 
 configure_corosync "$nodes_ip"
 start_corosync "$nodes_ip"
 
 cmap_set "$nodes_ip" "logging.debug" "str" "off"
 
-echo "EXIT" | run_app "$nodes_ip" 'testcpg' > /dev/null
+echo -e "sync\nexit" | run_app "$nodes_ip" 'cpg-cli-client'
 cat_corosync_log "$nodes_ip" | grep 'got procjoin message from cluster node' && exit 1
 
 exit 0
