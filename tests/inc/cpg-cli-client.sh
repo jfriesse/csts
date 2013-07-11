f50cfa0ab8c7f3fe45085ca6bed954274d5cd1be
Jan Friesse <jfriesse@redhat.com>
2013-07-11 17:36:25 +0200

    Enhance cpg-cli-client

--- tests/inc/cpg-cli-client.sh
+++ tests/inc/cpg-cli-client.sh
@@ -27,7 +27,7 @@ cpg_cli_client_send_run() {
     driver_run_app "stdin-to-usock" "$test_var_dir/cpg-cli-client-$node.sock"
 }
 
-# cpg_cli_client_send_run node
+# cpg_cli_client_exit node
 cpg_cli_client_exit() {
     local node="$1"
 
@@ -41,7 +41,36 @@ cpg_cli_client_cat_log() {
     cat "$test_var_dir/cpg-cli-client-$node.log"
 }
 
-# cpg_cli_client_cat_log node
+# cpg_cli_client_send_msg node msg
+cpg_cli_client_send_msg() {
+    local node="$1"
+    local msg="$2"
+
+    echo "sendstr $msg" | cpg_cli_client_send_run "$node"
+}
+
+
+# cpg_cli_client_wait_for_msg nodes msg
+cpg_cli_client_wait_for_msg() {
+    local nodes="$1"
+    local msg="$2"
+    local no_retries=0
+    local node
+
+    for node in $nodes;do
+        while ! cpg_cli_client_cat_log "$node" | \
+	  grep "^[0-9T]*:Arrived:([0-9a-z]* [0-9a-z]*):STR:[0-9]*:[0-9a-z]*:$msg\$" && [ $no_retries -lt 40 ];do
+            sleep 1
+            no_retries=$(($no_retries + 1))
+        done
+
+        [ "$no_retries" -lt 40 ] && true || return 1
+    done
+
+    return 0
+}
+
+# cpg_cli_client_wait_for_start node
 cpg_cli_client_wait_for_start() {
     local node="$1"
     local no_retries=0
