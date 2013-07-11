6c025a1c8c7e398c448c7e4a989fcee9dd9f097a
Jan Friesse <jfriesse@redhat.com>
2013-07-11 17:38:08 +0200

    Use cpg-cli-client in cpg-pause-confchg-first test

--- tests/cpg-pause-confchg-first.sh
+++ tests/cpg-pause-confchg-first.sh
@@ -8,60 +8,26 @@ test_required_nodes=2
 test_max_nodes=2
 
 . inc/common.sh
+. inc/cpg-cli-client.sh
 
-#wait_for_msg msg [redeliver]
-wait_for_msg() {
-    local msg="$1"
-    local redeliver="$2"
-    local all_delivered=false
-    local no_retries=0
-
-    while [ $no_retries -lt 40 ] && [ "$all_delivered" == false ];do
-        all_delivered=true
-
-        for node in $nodes_ip;do
-            if ! run "$node" "cat /tmp/testcpg.log" | grep "^DeliverCallback: message.*'$msg\$" &>/dev/null;then
-                all_delivered=false
-            fi
-        done
-
-        if [ "$all_delivered" == "false" ];then
-            sleep 0.5
-            no_retries=$(($no_retries + 1))
-            if [ "$redeliver" == true ];then
-                echo "$msg" >&60
-            fi
-        fi
-    done
-
-    [ "$all_delivered" == true ] && return 0 || return 1
-}
+cpg_cli_client_prepare "$nodes_ip"
 
 for node in $nodes_ip;do
     configure_corosync "$node"
     start_corosync "$node"
-    compile_app "$node" "testcpg" "-lcpg"
 done
 
-for ((i=0; i<$no_nodes; i++));do
-    for ((j=0; j<$no_nodes; j++));do
-        eval `echo 'exec '$((60+$j))'> >(run_app "${nodes_ip_array[$j]}" "testcpg" ">/tmp/testcpg.log")'`
-    done
+cpg_cli_client_start "$nodes_ip"
 
-    wait_for_msg "INIT PING" true
+for ((i=0; i<$no_nodes; i++));do
     pause_corosync "${nodes_ip_array[$i]}"
 
     # Wait for corosync to really pause
     sleep 5
 
-    eval `echo 'echo "PAUSE PONG" >&'$((60+$i))`
+    cpg_cli_client_send_msg "${nodes_ip_array[$i]}" "PAUSE PONG $i"
     unpause_corosync "${nodes_ip_array[$i]}"
-    wait_for_msg "PAUSE PONG" false
-
-    # Close file descriptors
-    for ((j=0; j<$no_nodes; j++));do
-        eval `echo 'exec '$((60+$j))'>&-'`
-    done
+    cpg_cli_client_wait_for_msg "$nodes_ip" "PAUSE PONG 2$i"
 done
 
 exit 0
