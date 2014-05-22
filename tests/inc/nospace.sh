542ebc1fd1c08b8cecd3ec3d8aeb6f8538ccb9f9
Jan Friesse <jfriesse@redhat.com>
2014-05-22 15:35:19 +0200

    Test corosync with no space in /dev/shm

---
+++ tests/inc/nospace.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# Author: Jan Friesse <jfriesse@redhat.com>
+#
+
+# Default path of image file (loop)
+nospace_image_file=${nospace_image_file:-"$test_var_dir/imgfile.img"}
+# Size of image (in kbytes)
+nospace_image_file_size=${nospace_image_file_size:-0}
+# Directory where to mount image to
+nospace_image_mount_point=${nospace_image_mount_point:-""}
+# File to used as fill file
+nospace_fill_file="$nospace_image_mount_point/fill"
+
+nospace_exit_trap_end_cb() {
+    return 0
+}
+
+exit_trap_end_cb() {
+    if run "$nodes_ip" mount | grep "$nospace_image_file";then
+        run "$nodes_ip" umount "$nospace_image_file"
+    fi
+
+    nospace_exit_trap_end_cb
+}
+
+# nospace_init node
+# Init nospace. Creates loop image, mounts it, ...
+nospace_init() {
+    local node="$1"
+
+    [ "$nospace_image_file_size" -gt 0 ] || return $?
+    [ "$nospace_image_mount_point" != "" ] || return $?
+
+    run "$node" dd if=/dev/zero of="$nospace_image_file" bs=1024 count=$nospace_image_file_size || return $?
+    run "$node" mkfs.ext2 -F "$nospace_image_file" || return $?
+    run "$node" mount "$nospace_image_file" "$nospace_image_mount_point" -o loop || return $?
+
+    return 0
+}
+
+# nospace_fill node
+nospace_fill() {
+    local node="$1"
+
+    run "$node" dd if=/dev/zero of="$nospace_fill_file" bs=1024 count=$nospace_image_file_size || true
+
+    return 0
+}
+
+# nospace_flush node
+nospace_flush() {
+    local node="$1"
+
+    run "$node" rm -f "$nospace_fill_file"
+
+    return 0
+}
