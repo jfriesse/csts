0397881706f60481ee2c2ba375862abc3dfc05a1
Jan Friesse <jfriesse@redhat.com>
2014-07-09 17:58:21 +0200

    Check fdata change in unlink-logsys-shm-buffers
    Fdata should change (new blackbox should be written) when ABRT signal is
    sent. We are testing it by using md5 sum of old fdata file compared to
    new (after ABRT signal) fdata file.

--- tests/unlink-logsys-shm-buffers.sh
+++ tests/unlink-logsys-shm-buffers.sh
@@ -7,6 +7,16 @@ test_description="Test that logsys shm buffers are correctly unlinked on signal 
 
 . inc/common.sh
 
+get_fdata_chsum() {
+    local chsum=0
+
+    if run "$nodes_ip" "[ -e '/var/lib/corosync/fdata' ]";then
+        chsum=`run "$nodes_ip" "md5sum '/var/lib/corosync/fdata'"`
+    fi
+
+    echo ${chsum%% *}
+}
+
 before_test=`run "$nodes_ip" "ls -1 /dev/shm/"`
 
 configure_corosync "$nodes_ip"
@@ -19,12 +29,20 @@ run "$nodes_ip" "corosync -V" && exit 1 || true
 after_test=`run "$nodes_ip" "ls -1 /dev/shm/"`
 [ "$before_test" == "$after_test" ]
 
+# Compute md5 sum of /var/lib/corosync/fdata if file exists
+chsum_start=`get_fdata_chsum`
 start_corosync "$nodes_ip"
 abrt_corosync "$nodes_ip"
 sleep 5
 stop_corosync "$nodes_ip" && exit 1 || true
 after_test=`run "$nodes_ip" "ls -1 /dev/shm/"`
 [ "$before_test" == "$after_test" ]
+chsum_end=`get_fdata_chsum`
+
+# Make sure that chsum of fdata at the beginning differs from chsum at the end. This means
+# that ether fdata didn't exist and now it does, or it is changed. If chsum didn't existed
+# and it still doesn't, chsums are equal and this test fails.
+[ "$chsum_start" != "$chsum_end" ]
 
 run "$nodes_ip" "killall -9 corosync || true; rm -f /var/run/corosync.pid"
 
